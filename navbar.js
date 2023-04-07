c1c65a502ee54c4a69c7578414e23cb3539de731
soorajsin <115391013+soorajsin@users.noreply.github.com>
2023-04-08 03:04:25 +0530

    main

--- navbar.js
+++ navbar.js
@@ -1,3 +1,14 @@
-function add(){
-          document.getElementById('product-add').value;
+window.onscroll = function () {
+          myFunction()
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+          if (window.pageYOffset >= sticky) {
+                    navbar.classList.add("sticky")
+          } else {
+                    navbar.classList.remove("sticky");
+          }
 }
